da1615c7b36649582f06bd56b717382cae499913
nimbleclouds <50649764+nimbleclouds@users.noreply.github.com>
2024-02-21 13:39:35 +0800

    Update fc.py

--- fc.py
+++ fc.py
@@ -14,6 +14,36 @@ from scipy.stats import gaussian_kde
 from streamlit_echarts import st_pyecharts
 from pyecharts.charts import Line, Bar, Scatter, Boxplot
 
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 df = pd.read_csv('df1.csv')
 inv = pd.read_csv('RemainingInventory.csv')
 items = pd.read_csv('ItemList.csv')
